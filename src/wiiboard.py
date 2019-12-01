3407dd5ac9144d3424325c2013505e8b859c82f4
Cristóbal Contreras Rubio <crisconru@gmail.com>
2019-12-01 16:50:42 +0000

    clase con wiiboard

--- src/wiiboard.py
+++ src/wiiboard.py
@@ -1,19 +1,36 @@
 import cwiid
 
-print('Conectando la wii')
-wiimote = None
-try:
-    wiimote = cwiid.Wiimote()
-    if wiimote:
-        print('conectado')
-        wiimote.led = cwiid.LED1_ON
-        # Activar la wii-board
-        wiimote.rpt_mode = cwiid.RPT_BALANCE        
-except RuntimeError:
-    print('error conectando')
-else:
-    print('Wiiboard lista')
-    rta = input('Pulse tecla para acabar')
-    wiimote.close()
-finally:
-    print('Adios')
+
+class WiiBoard:
+    def __init__(self):
+        self.wb = None
+
+    def conectar(self, intentos=3):
+        print(f'Wiiboard.conectar( {intentos} )')
+        if intentos:
+            try:
+                self.wb = cwiid.Wiimote()
+                if not self.wb:
+                    print('No se ha podido conectar, reintentando conexion')
+                    return self.conectar(intentos-1)
+                print(f'Estado = {self.wb.request_status()}')
+                self.wb.led = cwiid.LED1_ON
+                self.wb.rpt_mode = cwiid.RPT_BALANCE
+                print('Wiiboard conectada')
+                return True
+            except RuntimeError as err:
+                print(f'Error conectando = {err}')
+                return self.conectar(intentos-1)
+        print('Imposible conectar')
+
+    def desconectar(self):
+        print('Wiiboard.desconectar()')
+        self.wb.close()
+
+
+if __name__ == "__main__":
+    wb = WiiBoard()
+    input('Aprieta el botón de la Wii para conectar')
+    wb.conectar()
+    opcion = input('Pulse 1 para acabar')
+    wb.desconectar()
