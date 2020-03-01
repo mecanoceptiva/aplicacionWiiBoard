bf472dfb29f2cd2bdddd40fd3db77aede531f6fe
Cristóbal Contreras Rubio <crisconru@gmail.com>
2020-03-01 20:13:23 +0100

    arreglado el nombre de los ficheros

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,6 @@
 import logging
 from pathlib import Path
+from datetime import datetime
 
 BASE_DIR = Path(__file__).resolve().parents[0]
 LOG_DIR = BASE_DIR / 'logs'
@@ -8,8 +9,9 @@ LOG_DIR.mkdir(parents=True, exist_ok=True)
 logger = logging.getLogger('MisLogs')
 logger.setLevel(logging.DEBUG)
 # Crear handlers (donde van a ir los datos)
-datetime_filename = ':%Y-%m-%d %H:%M:%S'
-fh = logging.FileHandler(filename='{}.log'.format(datetime_filename))
+datetime_filename = datetime.now().strftime('%Y-%m-%d_%H:%M:%S') + '.log'
+log_file = str(LOG_DIR / datetime_filename)
+fh = logging.FileHandler(filename=log_file)
 fh.setLevel(logging.DEBUG)
 sh = logging.StreamHandler()
 sh.setLevel(logging.DEBUG)
