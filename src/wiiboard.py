a04a3df4ddc9308831662d931c1c23939ae838cd
Cristóbal Contreras Rubio <crisconru@gmail.com>
2020-03-01 17:06:41 +0100

    refactor

--- src/wiiboard.py
+++ src/wiiboard.py
@@ -5,19 +5,25 @@ class WiiBoard:
     def __init__(self):
         self.wb = None
 
+    def __conectar(self) -> bool:
+        self.wb = cwiid.Wiimote()
+        if self.wb:
+            print(f'Estado = {self.wb.request_status()}')
+            self.wb.led = cwiid.LED1_ON
+            self.wb.rpt_mode = cwiid.RPT_BALANCE
+            print('Wiiboard conectada')
+            return True
+        return False
+
     def conectar(self, intentos=3):
         print(f'Wiiboard.conectar( {intentos} )')
         if intentos:
             try:
-                self.wb = cwiid.Wiimote()
-                if not self.wb:
+                if self.__conectar():
+                    return True
+                else:
                     print('No se ha podido conectar, reintentando conexion')
-                    return self.conectar(intentos-1)
-                print(f'Estado = {self.wb.request_status()}')
-                self.wb.led = cwiid.LED1_ON
-                self.wb.rpt_mode = cwiid.RPT_BALANCE
-                print('Wiiboard conectada')
-                return True
+                    self.conectar(intentos-1)
             except RuntimeError as err:
                 print(f'Error conectando = {err}')
                 return self.conectar(intentos-1)
@@ -32,5 +38,5 @@ if __name__ == "__main__":
     wb = WiiBoard()
     input('Aprieta el botón de la Wii para conectar')
     wb.conectar()
-    opcion = input('Pulse 1 para acabar')
+    opcion = input('Pulse cualquier botón para acabar')
     wb.desconectar()
